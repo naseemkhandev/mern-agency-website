85db6d3d727ed0344da0c2f4b5f213afbcb7517f
Naseem Khan <naseemsaffy@gmail.com>
2024-05-05 20:19:49 +0500

    Add user registration route and controller

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import userRoute from './routes/authRoute.js';
 
+// Middlewares
 const app = express();
 app.use(express.json());
 
+// Routes
 app.use('/', (req, res) => {
   res.send('<h1>Server is running...</h1>');
 });
+
+app.use('/api/auth', userRoute);
 
 export default app;
---
+++ server/src/controllers/authController.js
@@ -0,0 +1,6 @@
+export const registerUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
