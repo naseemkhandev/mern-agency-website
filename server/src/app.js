83c970791486d65ee8bb59f8a990b9479d05cdf6
Naseem Khan <naseemsaffy@gmail.com>
2024-05-05 20:15:30 +0500

    Add basic server setup and routing

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+app.use('/', (req, res) => {
+  res.send('<h1>Server is running...</h1>');
+});
 
 export default app;
