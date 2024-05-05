41515fdafe99d070135f2ca7dab6f3d18b7d9922
Naseem Khan <naseemsaffy@gmail.com>
2024-05-05 19:59:57 +0500

    add configuration file to use env variables

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-import app from "./src/app.js";
+import app from './src/app.js';
+import { config } from './src/config/config.js';
 
 const startServer = async () => {
   try {
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
