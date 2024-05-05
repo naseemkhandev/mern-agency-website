ea1f6bc9da44f54d80fbef6b0941b043b84148c3
Naseem Khan <naseemsaffy@gmail.com>
2024-05-05 22:36:21 +0500

    Add .env.example files and update Contact component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,117 +4,117 @@ import { Link } from "react-router-dom";
 import { loginBg } from "../assets";
 
 const Login = () => {
-	const [user, setUser] = useState({
-		username: "",
-		password: "",
-	});
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-		setUser((preValue) => ({ ...preValue, [name]: value }));
-	};
+    setUser((preValue) => ({ ...preValue, [name]: value }));
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-	return (
-		<section
-			style={{
-				background: `url(${loginBg})`,
-				backgroundSize: "cover",
-				backgroundPosition: "center",
-			}}
-		>
-			<div className="flex bg-black/50 py-20 h-full flex-col items-center your justify-center your px-6 mx-auto">
-				<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 md:w-[25rem] xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-					<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-						<h1 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-							Sign in to your account
-						</h1>
-						<form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-							<div>
-								<label
-									htmlFor="username"
-									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-								>
-									Username
-								</label>
-								<input
-									type="username"
-									name="username"
-									id="username"
-									value={user.username}
-									onChange={handleChange}
-									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-900 focus:border-slate-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-									placeholder="Enter your username"
-								/>
-							</div>
-							<div>
-								<label
-									htmlFor="password"
-									className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-								>
-									Password
-								</label>
-								<input
-									type="password"
-									name="password"
-									id="password"
-									value={user.password}
-									onChange={handleChange}
-									placeholder="Enter your password"
-									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-900 focus:border-slate-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-								/>
-							</div>
-							<div className="flex items-center justify-between">
-								<div className="flex items-start">
-									<div className="flex items-center h-5">
-										<input
-											id="remember"
-											aria-describedby="remember"
-											type="checkbox"
-											className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-slate-900 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-slate-900 dark:ring-offset-gray-800"
-										/>
-									</div>
-									<div className="ml-3 text-sm">
-										<label
-											htmlFor="remember"
-											className="text-gray-500 dark:text-gray-300"
-										>
-											Remember me
-										</label>
-									</div>
-								</div>
-								<Link
-									to="/login"
-									className="text-sm font-medium text-slate-900 hover:underline dark:text-slate-900"
-								>
-									Forgot password?
-								</Link>
-							</div>
-							<button
-								type="submit"
-								className="w-full text-white bg-slate-900 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-900 font-medium rounded-lg text-sm px-5 py-3.5 text-center dark:bg-slate-900 dark:hover:bg-slate-800 dark:focus:ring-slate-900"
-							>
-								Sign in
-							</button>
-							<p className="text-sm font-light text-gray-500 dark:text-gray-400">
-								Don&apos;t have an account yet?{" "}
-								<Link
-									to="/register"
-									className="font-medium text-slate-900 hover:underline dark:text-slate-900"
-								>
-									Register
-								</Link>
-							</p>
-						</form>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section
+      style={{
+        background: `url(${loginBg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="flex bg-black/50 py-20 h-full flex-col items-center your justify-center your px-6 mx-auto">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 md:w-[25rem] xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-semibold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={user.email}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-900 focus:border-slate-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={user.password}
+                  onChange={handleChange}
+                  placeholder="Enter your password"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-slate-900 focus:border-slate-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-slate-900 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-slate-900 dark:ring-offset-gray-800"
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="remember"
+                      className="text-gray-500 dark:text-gray-300"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <Link
+                  to="/login"
+                  className="text-sm font-medium text-slate-900 hover:underline dark:text-slate-900"
+                >
+                  Forgot password?
+                </Link>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-slate-900 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-900 font-medium rounded-lg text-sm px-5 py-3.5 text-center dark:bg-slate-900 dark:hover:bg-slate-800 dark:focus:ring-slate-900"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don&apos;t have an account yet?{" "}
+                <Link
+                  to="/register"
+                  className="font-medium text-slate-900 hover:underline dark:text-slate-900"
+                >
+                  Register
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
