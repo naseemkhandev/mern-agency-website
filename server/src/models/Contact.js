6668611828ad4fa2f7201aff98de5aedc98f5255
Naseem Khan <naseemsaffy@gmail.com>
2024-05-05 22:11:22 +0500

    Add Contact model for handling user messages

---
+++ server/src/models/Contact.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Contact = mongoose.model('Contact', contactSchema);
+export default Contact;
