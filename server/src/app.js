224d6d90284164137f25b34a8aa4a953553578d2
Naseem Khan <naseemsaffy@gmail.com>
2024-05-05 20:52:11 +0500

    create user in database

--- server/src/app.js
+++ server/src/app.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/', (req, res) => {
-  res.send('<h1>Server is running...</h1>');
+app.get('/', (req, res) => {
+  res.json('<h1>Server is running...</h1>');
 });
 
 app.use('/api/auth', userRoute);
--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,17 @@
+import User from '../models/User.js';
+
 export const registerUser = async (req, res) => {
   try {
+    const { username, email, phone, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: 'User already exists' });
+
+    const newUser = await User.create({ username, email, phone, password });
+
+    res
+      .status(201)
+      .json({ message: 'User registered successfully', user: newUser });
   } catch (error) {
     console.log(error);
   }
